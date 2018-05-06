c884576485c7dd5d0916b6acd94d49f6f802620a
Mattia Barbon <mattia@barbon.org>
2018-05-06 13:57:00 +0200

    Add basic compile and sanity tests

--- t/lib/LoadCamelcade.pm
+++ t/lib/LoadCamelcade.pm
@@ -5,7 +5,21 @@ use warnings;
 
 use File::Basename qw();
 
-use blib File::Basename::dirname($INC{'LoadCamelcade.pm'});
-use Apache2::Camelcadedb remote_host => 'localhost:12345';
+my ($DIR, $PORT, $ADDRESS);
+BEGIN {
+    $DIR = File::Basename::dirname($INC{'LoadCamelcade.pm'}) . "/../..";
+    $PORT = 23456;
+    # $PORT = 12345;
+    $ADDRESS = "localhost:$PORT";
+}
+
+use blib $DIR;
+use Apache2::Camelcadedb remote_host => $ADDRESS;
+
+{
+    open my $fh, '>', "$DIR/t/logs/camelcade_port.txt"
+        or die "Error opening '$DIR/t/logs/camelcade_port.txt': $!";
+    print $fh $ADDRESS;
+}
 
 1;
